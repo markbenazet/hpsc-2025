8f380cf09e9284b105ce32c3e1b5d89a637b1cb0
markbenazet <mbenazet@student.ethz.ch>
2025-04-25 12:57:21 +0900

    Added few exercises which I have been doing lately

---
+++ 06_cuda/13_bucket_sort.cu
@@ -0,0 +1,101 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <cuda_runtime.h>
+
+__global__ void countElements(int *key, int n, int range, int *blockCounts) {
+  extern __shared__ int localBucket[];
+  
+  for (int i = threadIdx.x; i < range; i += blockDim.x) {
+      localBucket[i] = 0;
+  }
+  __syncthreads();
+  
+  for (int i = threadIdx.x + blockIdx.x * blockDim.x; i < n; i += blockDim.x * gridDim.x) {
+      atomicAdd(&localBucket[key[i]], 1);
+  }
+  __syncthreads();
+  
+  for (int i = threadIdx.x; i < range; i += blockDim.x) {
+      blockCounts[blockIdx.x * range + i] = localBucket[i];
+  }
+}
+
+__global__ void sortElements(int *key, int n, int range, int *blockCounts, int *output) {
+  extern __shared__ int sharedMem[];
+  int* localBucket = sharedMem;
+  int* globalOffset = &sharedMem[range];
+  
+  for (int i = threadIdx.x; i < range; i += blockDim.x) {
+      localBucket[i] = 0;
+      globalOffset[i] = 0;
+  }
+  __syncthreads();
+  
+  for (int i = threadIdx.x + blockIdx.x * blockDim.x; i < n; i += blockDim.x * gridDim.x) {
+      atomicAdd(&localBucket[key[i]], 1);
+  }
+  __syncthreads();
+  
+  if (threadIdx.x == 0) {
+      for (int b = 0; b < range; b++) {
+          int globalPos = 0;
+          for (int blk = 0; blk < blockIdx.x; blk++) {
+              globalPos += blockCounts[blk * range + b];
+          }
+          globalOffset[b] = globalPos;
+      }
+  }
+  __syncthreads();
+  
+  if (threadIdx.x == 0) {
+      int sum = 0;
+      for (int b = 0; b < range; b++) {
+          int temp = localBucket[b];
+          localBucket[b] = sum;
+          sum += temp;
+      }
+  }
+  __syncthreads();
+  
+  for (int i = threadIdx.x + blockIdx.x * blockDim.x; i < n; i += blockDim.x * gridDim.x) {
+      int val = key[i];
+      int pos = globalOffset[val] + localBucket[val];
+      atomicAdd(&localBucket[val], 1);
+      output[pos] = val;
+  }
+}
+
+int main() {
+  int n = 50;
+  int range = 5;
+  std::vector<int> key(n);
+  for (int i=0; i<n; i++) {
+    key[i] = rand() % range;
+    printf("%d ",key[i]);
+  }
+  
+  int *d_key, *d_output, *d_blockCounts;
+  cudaMalloc(&d_key, n * sizeof(int));
+  cudaMalloc(&d_output, n * sizeof(int));
+  cudaMalloc(&d_blockCounts, numBlocks * range * sizeof(int));
+  
+  cudaMemcpy(d_key, key.data(), n * sizeof(int), cudaMemcpyHostToDevice);
+  
+  countElements<<<numBlocks, 256, range * sizeof(int)>>>(d_key, n, range, d_blockCounts);
+  
+  sortElements<<<numBlocks, 256, 2 * range * sizeof(int)>>>(d_key, n, range, d_blockCounts, d_output);
+  
+  cudaDeviceSynchronize();
+  
+  cudaMemcpy(key.data(), d_output, n * sizeof(int), cudaMemcpyDeviceToHost);
+  
+  cudaFree(d_key);
+  cudaFree(d_output);
+  cudaFree(d_blockCounts);
+
+  for (int i=0; i<n; i++) {
+    printf("%d ",key[i]);
+  }
+  printf("\n");
+}
--- 13_scientific/10_cavity.cpp
+++ 13_scientific/10_cavity.cpp
@@ -38,7 +38,18 @@ int main() {
   for (int n=0; n<nt; n++) {
     for (int j=1; j<ny-1; j++) {
       for (int i=1; i<nx-1; i++) {
-        // Compute b[j][i]
+        
+        double du_dx = (u[j][i+1] - u[j][i-1]) / (2 * dx);
+        double dv_dy = (v[j+1][i] - v[j-1][i]) / (2 * dy);
+        double du_dy = (u[j+1][i] - u[j-1][i]) / (2 * dy);
+        double dv_dx = (v[j][i+1] - v[j][i-1]) / (2 * dx);
+        
+        double div_term = (du_dx + dv_dy) / dt;       // Divergence term (1/dt * (du/dx + dv/dy))
+        double squared_u_term = du_dx * du_dx;        // (du/dx)²
+        double cross_term = 2 * du_dy * dv_dx;        // 2 * du/dy * dv/dx
+        double squared_v_term = dv_dy * dv_dy;        // (dv/dy)²
+        
+        b[j][i] = rho * (div_term - squared_u_term - cross_term - squared_v_term);
       }
     }
     for (int it=0; it<nit; it++) {
@@ -47,32 +58,69 @@ int main() {
 	  pn[j][i] = p[j][i];
       for (int j=1; j<ny-1; j++) {
         for (int i=1; i<nx-1; i++) {
-	  // Compute p[j][i]
+            
+            double pressure_x_term = dy*dy * (pn[j][i+1] + pn[j][i-1]);  
+            double pressure_y_term = dx*dx * (pn[j+1][i] + pn[j-1][i]);  
+            double denominator = 2 * (dx*dx + dy*dy);                    
+            double source_term = dx*dx * dy*dy * b[j][i];
+            
+            p[j][i] = (pressure_x_term + pressure_y_term - source_term) / denominator;
 	}
       }
       for (int j=0; j<ny; j++) {
-        // Compute p[j][0] and p[j][nx-1]
+        p[j][0] = p[j][1];
+        p[j][nx-1] = p[j][nx-2];
       }
       for (int i=0; i<nx; i++) {
-	// Compute p[0][i] and p[ny-1][i]
+        p[0][i] = p[1][i];
+        p[ny-1][i] = 0; 
       }
     }
     for (int j=0; j<ny; j++) {
       for (int i=0; i<nx; i++) {
         un[j][i] = u[j][i];
-	vn[j][i] = v[j][i];
+	      vn[j][i] = v[j][i];
       }
     }
     for (int j=1; j<ny-1; j++) {
       for (int i=1; i<nx-1; i++) {
-	// Compute u[j][i] and v[j][i]
+        
+        double u_prev = un[j][i];
+        
+        double u_conv_x = un[j][i] * dt / dx * (un[j][i] - un[j][i-1]);
+        
+        double u_conv_y = un[j][i] * dt / dy * (un[j][i] - un[j-1][i]);
+        
+        double u_press = dt / (2 * rho * dx) * (p[j][i+1] - p[j][i-1]);
+        
+        double u_diff = nu * (dt / (dx*dx) * (un[j][i+1] - 2 * un[j][i] + un[j][i-1]) +
+                   dt / (dy*dy) * (un[j+1][i] - 2 * un[j][i] + un[j-1][i]));
+        u[j][i] = u_prev - u_conv_x - u_conv_y - u_press + u_diff;
+
+        double v_prev = vn[j][i];
+
+        double v_conv_x = vn[j][i] * dt / dx * (vn[j][i] - vn[j][i-1]);
+
+        double v_conv_y = vn[j][i] * dt / dy * (vn[j][i] - vn[j-1][i]);
+
+        double v_press = dt / (2 * rho * dy) * (p[j+1][i] - p[j-1][i]);
+
+        double v_diff = nu * (dt / (dx*dx) * (vn[j][i+1] - 2 * vn[j][i] + vn[j][i-1]) +
+                   dt / (dy*dy) * (vn[j+1][i] - 2 * vn[j][i] + vn[j-1][i]));
+        v[j][i] = v_prev - v_conv_x - v_conv_y - v_press + v_diff;
       }
     }
     for (int j=0; j<ny; j++) {
-      // Compute u[j][0], u[j][nx-1], v[j][0], v[j][nx-1]
+      u[j][0] = 0;
+      u[j][nx-1] = 0;
+      v[j][0] = 0;
+      v[j][nx-1] = 0;
     }
     for (int i=0; i<nx; i++) {
-      // Compute u[0][i], u[ny-1][i], v[0][i], v[ny-1][i]
+      u[0][i] = 0;
+      u[ny-1][i] = 1;
+      v[0][i] = 0;
+      v[ny-1][i] = 0;
     }
     if (n % 10 == 0) {
       for (int j=0; j<ny; j++)
