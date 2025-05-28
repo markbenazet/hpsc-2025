ef4a608e7380f9f1b59956348cfe72e4732c472f
Mark Benazet Castells <145600765+markbenazet@users.noreply.github.com>
2025-05-28 16:30:44 +0900

    Cleaned up reading

--- 09_io/17_phdf5_read.cpp
+++ 09_io/17_phdf5_read.cpp
@@ -12,35 +12,43 @@ int main (int argc, char** argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
   assert(mpisize == dim[0]*dim[1]);
-  hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
-  H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
-  hid_t file = H5Fopen("data.h5", H5F_ACC_RDONLY, plist);
+  hid_t plist_fapl = H5Pcreate(H5P_FILE_ACCESS);
+  H5Pset_fapl_mpio(plist_fapl, MPI_COMM_WORLD, MPI_INFO_NULL);
+  hid_t file = H5Fopen("data.h5", H5F_ACC_RDONLY, plist_fapl);
+  H5Pclose(plist_fapl);
+  
   hid_t dataset = H5Dopen(file, "dataset", H5P_DEFAULT);
   hid_t globalspace = H5Dget_space(dataset);
+  
   int ndim = H5Sget_simple_extent_ndims(globalspace);
   hsize_t N[ndim];
   H5Sget_simple_extent_dims(globalspace, N, NULL);
   hsize_t NX = N[0], NY = N[1];
+  
   hsize_t Nlocal[2] = {NX/2, NY/2};
   hsize_t offset[2] = {mpirank / dim[1], mpirank % dim[1]};
   hsize_t block[2] = {1,1};
   hsize_t stride[2] = {2,2};
   hsize_t count[2] = {Nlocal[0], Nlocal[1]};
-  hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
-  hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
+  
   H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, block);
-  H5Pclose(plist);
+  
+  hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
+  
   vector<int> buffer(Nlocal[0]*Nlocal[1]);
-  plist = H5Pcreate(H5P_DATASET_XFER);
-  H5Pset_dxpl_mpio(plist, H5FD_MPIO_COLLECTIVE);
+  
+  hid_t plist_dxpl = H5Pcreate(H5P_DATASET_XFER);
+  
+  H5Pset_dxpl_mpio(plist_dxpl, H5FD_MPIO_COLLECTIVE);
+  
   auto tic = chrono::steady_clock::now();
-  H5Dread(dataset, H5T_NATIVE_INT, localspace, globalspace, plist, &buffer[0]);
+  H5Dread(dataset, H5T_NATIVE_INT, localspace, globalspace, plist_dxpl, &buffer[0]);
   auto toc = chrono::steady_clock::now();
+  H5Pclose(plist_dxpl);
   H5Dclose(dataset);
   H5Sclose(localspace);
   H5Sclose(globalspace);
   H5Fclose(file);
-  H5Pclose(plist);
   double time = chrono::duration<double>(toc - tic).count();
   int sum = 0;
   for (int i=0; i<Nlocal[0]*Nlocal[1]; i++)
