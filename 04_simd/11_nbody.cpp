41765191025e5e0de7e97fe0756e90188e9d7de0
markbenazet <mbenazet@student.ethz.ch>
2025-05-28 17:23:23 +0900

    Cleaning up simd parallelization

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -4,54 +4,54 @@
 #include <immintrin.h> 
 
 int main() {
-  const int N = 8;
-  alignas(32) float x[N], y[N], m[N], fx[N], fy[N]; 
+  const int N = 16;
+  alignas(64) float x[N], y[N], m[N], fx[N], fy[N]; 
   
   for(int i=0; i<N; i++) {
-    x[i] = drand48();
-    y[i] = drand48();
-    m[i] = drand48();
-    fx[i] = fy[i] = 0;
+    x[i] = (float)drand48();
+    y[i] = (float)drand48();
+    m[i] = (float)drand48() + 0.1f;
+    fx[i] = fy[i] = 0.0f;
   }
+
+    __m512 x_vec = _mm512_load_ps(x);
+    __m512 y_vec = _mm512_load_ps(y);
+    __m512 m_vec = _mm512_load_ps(m);
+
+    __m512 zero_vec = _mm512_setzero_ps();
+    __m512 epsilon_sq_vec = _mm512_set1_ps(1e-6f); 
+
   
   for(int i=0; i<N; i++) {
-    __m256 xi = _mm256_set1_ps(x[i]);
-    __m256 yi = _mm256_set1_ps(y[i]);
-  
-    __m256 xj = _mm256_load_ps(x);  
-    __m256 yj = _mm256_load_ps(y);  
-    __m256 mj = _mm256_load_ps(m);  
+
+    __m512 xi = _mm512_set1_ps(x[i]);
+    __m512 yi = _mm512_set1_ps(y[i]);
     
-    __m256 rx = _mm256_sub_ps(xi, xj);  
-    __m256 ry = _mm256_sub_ps(yi, yj);  
+    __m512 rx_vec = _mm512_sub_ps(x_vec, xi);
+    __m512 ry_vec = _mm512_sub_ps(y_vec, yi);
+    __m512 r2_vec = _mm512_fmadd_ps(rx_vec, rx_vec, _mm512_mul_ps(ry_vec, ry_vec));
+
+    __mmask16 self_interaction_mask = (1 << i);
+    __mmask16 r_too_small_mask = _mm512_cmp_ps_mask(r2_vec, epsilon_sq_vec, _CMP_LT_OQ);
+    __mmask16 ignore_force_mask = self_interaction_mask | r_too_small_mask;
+
+    __m512 inv_r_vec = _mm512_rsqrt14_ps(r2_vec);
     
-    __m256 rx2 = _mm256_mul_ps(rx, rx);  
-    __m256 ry2 = _mm256_mul_ps(ry, ry);  
-    __m256 r2 = _mm256_add_ps(rx2, ry2); 
-    
-    __m256 r = _mm256_sqrt_ps(r2);          
-    __m256 r3 = _mm256_mul_ps(r2, r);       
-    
-    __m256 mask = _mm256_cmp_ps(xi, xj, _CMP_NEQ_OQ);  
-    __m256 r3_safe = _mm256_blendv_ps(_mm256_set1_ps(1.0f), r3, mask); 
-    
-    __m256 invr3 = _mm256_div_ps(_mm256_set1_ps(1.0f), r3_safe);  
-    __m256 coef = _mm256_mul_ps(mj, invr3);  
-    
-    coef = _mm256_and_ps(coef, mask);  
-    
-    __m256 fx_incr = _mm256_mul_ps(rx, coef);  
-    __m256 fy_incr = _mm256_mul_ps(ry, coef);  
-    
-    alignas(32) float fx_array[8], fy_array[8];
-    _mm256_store_ps(fx_array, fx_incr);
-    _mm256_store_ps(fy_array, fy_incr);
-    
-    for(int j=0; j<N; j++) {
-      fx[i] -= fx_array[j];
-      fy[i] -= fy_array[j];
-    }
-    
+    __m512 inv_r3_vec = _mm512_mul_ps(_mm512_mul_ps(inv_r_vec, inv_r_vec), inv_r_vec);
+
+    __m512 coef_vec = _mm512_mul_ps(m_vec, inv_r3_vec);
+
+    __m512 fx_components_vec = _mm512_mul_ps(rx_vec, coef_vec);
+    __m512 fy_components_vec = _mm512_mul_ps(ry_vec, coef_vec);
+
+    fx_components_vec = _mm512_mask_blend_ps(ignore_force_mask, fx_components_vec, zero_vec);
+    fy_components_vec = _mm512_mask_blend_ps(ignore_force_mask, fy_components_vec, zero_vec);
+
+    fx[i] = _mm512_reduce_add_ps(fx_components_vec);
+    fy[i] = _mm512_reduce_add_ps(fy_components_vec);
+  }
+
+  for(int i=0; i<N; i++) {
     printf("%d %g %g\n", i, fx[i], fy[i]);
   }
   
